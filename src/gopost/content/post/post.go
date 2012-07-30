95ac7d1a1b8132da6678d6bd71e625cc8cbb5273
Greg McClure <gmcclure@gmail.com>
2012-07-29 22:50:56 -0700

    Added some docstrings.

--- src/gopost/content/post/post.go
+++ src/gopost/content/post/post.go
@@ -8,11 +8,13 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Post is, along with Page, a fundamental unit of content in the blog.
 type Post struct {
 	Title string
 	Body  []byte
 }
 
+// Save uses dbDriver and dbName strings to save a post to the database.
 func (p *Post) Save(dbDriver, dbName string) error {
 	db, err := sql.Open(dbDriver, dbName)
 	if err != nil {
