d25495b1c28e9b5c1b346867a3eca75f5799ce19
Greg McClure <gmcclure@gmail.com>
2012-08-16 12:31:06 -0700

    Adding a functional index template.

--- src/gopost/main/gopost.go
+++ src/gopost/main/gopost.go
@@ -1,22 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"gopost/content/post"
 	"html/template"
 	"net/http"
-	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("tmpl/index.html")
-	p := &Post{Title: "Falling Rockets", Body: []byte("Hello, Professor McClure.")}
 	if err != nil {
-		cwd, _ := os.Getwd()
-		fmt.Fprintf(w, "Template not available: %v. Current working dir: %v", err, cwd)
-	} else {
-		t.Execute(w, p)
+		panic(err)
 	}
+	ctx := make(map[string]*[]post.Post)
+	ctx["posts"] = post.GetAll()
+	t.Execute(w, ctx)
 }
 
 func main() {
